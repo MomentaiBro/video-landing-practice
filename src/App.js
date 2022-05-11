ae118d1aa34b1b4a2cd43a95a650dda82c6e66c1
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-10 21:58:46 -0600

    got the video to show on screen AND got it to loop WOOHOO

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-    Hello 
+      <header className="v-header container">
+        <div className='fullscreen-video-wrap'>
+          <video src='../assets/grass-in-wind.mov' autoPlay loop muted="true" ></video>
+        </div>
       </header>
     </div>
   );
