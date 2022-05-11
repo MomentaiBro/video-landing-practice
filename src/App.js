c51ba343111f6337db78863938a81e47aada2cfd
Alexander Sherwin <alx.sherwin@gmail.com>
2022-05-10 22:08:25 -0600

    html is finished and working on css styling right now

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
         <div className='fullscreen-video-wrap'>
           <video src='../assets/grass-in-wind.mov' autoPlay loop muted="true" ></video>
         </div>
+        <div className='header-overlay'> </div>
+        <div className='header-content'> 
+          <h1> Momentai </h1>
+          <p>Take it easy</p>
+          <a href="/" className='btn'> Read More</a>
+        </div>
       </header>
     </div>
   );
